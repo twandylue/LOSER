2ec3fe07dab58d996127b08739fddda6fee6da61
AndyLu <laserlue@gmail.com>
2023-02-23 15:49:02 +0800

    feat: readers
    Add plain_text_reader for reading .txt files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use lexer::Lexer;
+use reader::plain_text_reader::{PlainTextReader, Reader};
+use std::path::Path;
+
+mod lexer;
+mod reader;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = Path::new("./src/data/test.txt");
+
+    let result = PlainTextReader::read_text(file_path)?;
+    let chars = &result.chars().collect::<Vec<char>>();
+    let lexer = Lexer::new(chars);
+
+    println!("{:?}", lexer.content);
+
+    Ok(())
 }
---
+++ src/reader/plain_text_reader.rs
@@ -0,0 +1,23 @@
+use std::{fs::File, io::Read, path::Path};
+
+type Error = Box<dyn std::error::Error>;
+
+pub trait Reader {
+    fn read_text(file_path: &Path) -> Result<String, Error>;
+}
+
+pub struct PlainTextReader {}
+
+impl Reader for PlainTextReader {
+    fn read_text(file_path: &Path) -> Result<String, Error> {
+        let mut content = String::new();
+
+        let mut buf = String::new();
+        let mut file = File::open(file_path)?;
+        file.read_to_string(&mut buf)?;
+
+        // buf.split(" ").for_each(|x| content.push_str(x));
+
+        Ok(buf)
+    }
+}
