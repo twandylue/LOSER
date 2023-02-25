291e52e3af4f7668282f33c1caac1416ee879da7
AndyLu <laserlue@gmail.com>
2023-02-25 10:59:57 +0800

    refactor: refactor main.rs
    Refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use lexer::Lexer;
 use reader::plain_text_reader::{PlainTextReader, Reader};
 use serde_json;
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
 
 mod lexer;
 mod reader;
@@ -13,35 +16,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let paths = std::fs::read_dir("./src/data")?;
     let mut file_tf = FileTF::new();
 
+    println!("Indexing...");
     for path in paths {
         let file_path = path?.path();
+        println!("File: {file_path:?}");
+        let result = read_from_file(&file_path)?;
+        let tf = create_tf(result);
 
-        let result = PlainTextReader::read_text(&file_path)?;
-        let chars = &result.chars().collect::<Vec<char>>();
-        let lexer = Lexer::new(chars);
+        // let mut sorted_vec = tf.iter().collect::<Vec<(&String, &usize)>>();
+        // sorted_vec.sort_by(|a, b| b.1.cmp(a.1));
+        //
+        // for (k, v) in sorted_vec.iter().take(10) {
+        //     println!("  {k} => {v}");
+        // }
 
-        let mut counter = TermFreq::new();
-
-        lexer.for_each(|x| {
-            counter.entry(x).and_modify(|c| *c += 1).or_insert(1);
-        });
-
-        let mut sorted_vec = counter.iter().collect::<Vec<(&String, &usize)>>();
-        sorted_vec.sort_by(|a, b| b.1.cmp(a.1));
-
-        println!("{file_path:?}");
-        for (k, v) in sorted_vec.iter().take(10) {
-            println!("  {k} => {v}");
-        }
-
-        file_tf.insert(file_path, counter);
-        println!();
+        file_tf.insert(file_path, tf);
     }
 
     let output = serde_json::to_string(&file_tf)?;
-    println!("{output}");
 
     std::fs::write("./src/index.json", output)?;
 
     Ok(())
+}
+
+fn read_from_file(file: &Path) -> Result<String, Box<dyn std::error::Error>> {
+    match file.extension().unwrap().to_str() {
+        Some("txt") => Ok(PlainTextReader::read_text(file)?),
+        _ => Err("Error in reader.".into()),
+    }
+}
+
+fn create_tf(content: String) -> TermFreq {
+    let chars = &content.chars().collect::<Vec<char>>();
+    let lexer = Lexer::new(chars);
+
+    let mut counter = TermFreq::new();
+
+    lexer.for_each(|x| {
+        counter.entry(x).and_modify(|c| *c += 1).or_insert(1);
+    });
+
+    counter
 }
